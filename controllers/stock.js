0ac462d7f0b2a2ae982d9ce15f325f0cccd8fcbd
Tim3Ds <Tim3Ds@gmail.com>
2017-04-10 19:20:34 -0600

    working stock pages

---
+++ controllers/stock.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+// has function to make sure you can only get to page if logged in
+var utils = require('./utils');  
+// NOTE THE utils.requireLogin   
+// this function from the utils class makes sure they are logged 
+// in before being able to go to chat but its not working right
+
+/* GET stock page. */
+router.get('/view', utils.requireLogin, function(req, res, next) {
+  res.render('view', {
+    userName: req.user.username,
+    csrfToken: req.csrfToken()
+  });
+});
+
+router.get('/list', utils.requireLogin, function(req, res, next) {
+  res.render('list', {
+    userName: req.user.username,
+    csrfToken: req.csrfToken()
+  });
+});
+
+router.get('/add', utils.requireLogin, function(req, res, next) {
+  res.render('add', {
+    userName: req.user.username,
+    csrfToken: req.csrfToken()
+  });
+});
+
+router.get('/manage', utils.requireLogin, function(req, res, next) {
+  res.render('manage', {
+    userName: req.user.username,
+    csrfToken: req.csrfToken()
+  });
+});
+
+module.exports = router;
