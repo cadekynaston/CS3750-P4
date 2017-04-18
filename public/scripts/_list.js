fa0f6ef13a48a184c46813110eb1b4b0180b310d
Alex <mustang.4222@yahoo.com>
2017-04-18 14:37:57 -0600

    yeah

--- public/scripts/_list.js
+++ public/scripts/_list.js
@@ -5,14 +5,12 @@ window.onload = ()=> {
         $('tbody').append('<tr><th>' + element.stockCode + '</th><td>' + element.amount + '</td>'
         + '<td>Up</td><td>TC</td><td><input type="button" id="removebtn" class="btn btn-danger" value="Remove" onclick="remove(this)"/></td>><hr/>');
     })
-
 }
+
 function remove(item)
-{
-    
+{    
     $('table').on('click', 'input[type="button"]', function(e)
     {
         $(this).closest('tr').remove();
-    }
-    );
+    });
 }
