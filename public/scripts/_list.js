fdab9b4802f1c03e2d4d2870f6915ecd0a89dbdc
Alex <mustang.4222@yahoo.com>
2017-04-17 19:18:50 -0600

    playing around with temp stocks

--- public/scripts/_list.js
+++ public/scripts/_list.js
@@ -1,6 +1,14 @@
 window.onload = ()=> {
-     //$('.stock-list').append('<div><label><input type="checkbox"/> hello</label></div>');
+     var stocks = [
+            {stockCode:"EFGH", amount: 44},
+            {stockCode:"IJKL", amount: 34},
+            {stockCode:"MNOP", amount: 12},
+            {stockCode:"ABCD", amount: 10}
+            ];
+
      var portfolio = $('#portfolio').portfolio;
-     $('.stock-list').append('<div>' + portfolio + '</div>')
+     stocks.forEach(function(element) {
+        $('.stock-list').append('<div id="list"><span id="left">' + element.stockCode + '</span><span id="right">' + element.amount + '</span></div> <hr/> ');        
+    })
 
 }
