2ec34e8350139ec94c11646ec6de01b0c09585f4
Alex <mustang.4222@yahoo.com>
2017-04-18 11:23:03 -0600

    added remove button to list page

--- public/scripts/_list.js
+++ public/scripts/_list.js
@@ -1,7 +1,8 @@
 window.onload = ()=> {
      var portfolio = JSON.parse($('#portfolio').val());
      portfolio.forEach(function(element) {
-        $('.stock-list').append('<div id="list"><span id="left">' + element.stockCode + '</span><span id="right">' + element.amount + '</span></div> <hr/> ');        
+        $('.stock-list').append('<div id="list"><span id="left">' + element.stockCode + '</span><span id="right">' + element.amount + '</span><input type="button" id="removebtn" class="btn btn-danger" value="Remove"/></div><hr/>');
+        $('.stock-list').append();
     })
 
 }
