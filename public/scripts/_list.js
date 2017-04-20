1d3760dfc8a3ed4baff003b6aeb803ede00dd4d4
Alex <mustang.4222@yahoo.com>
2017-04-19 18:55:09 -0600

    working on calling API to get price and status

--- public/scripts/_list.js
+++ public/scripts/_list.js
@@ -2,12 +2,31 @@
 window.onload = ()=> {
      var portfolio = JSON.parse($('#portfolio').val());
      portfolio.forEach(function(element) {
-         // add stockName to portfolio for easy access, maybe add current price too
-         // they arent in the portfolio that's why they are undefined on the screen
+        //console.log(element.stockCode);
+        callAPI(element.stockCode);
         $('tbody').append('<tr><th>' + element.stockTitle + '</th><td>' + element.price + '</td>'
         + '<td>Up</td><td id="stockCode">' + element.stockCode + '</td><td><input type="button" id="removebtn" class="btn btn-danger" value="Remove" onclick="remove(this)"/></td>><hr/>');
-    })
+    })    
 }
+
+function jsonCallback(data){
+    console.log(data.Change);
+    console.log(data.Name);
+    console.log(data.LastPrice);
+    // add stockName to portfolio for easy access, maybe add current price too
+    // they arent in the portfolio that's why they are undefined on the screen
+    // $('tbody').append('<tr><th>' + element.stockTitle + '</th><td>' + element.price + '</td>'
+    // + '<td>Up</td><td id="stockCode">' + element.stockCode + '</td><td><input type="button" id="removebtn" class="btn btn-danger" value="Remove" onclick="remove(this)"/></td>><hr/>');
+            
+};
+            
+function callAPI(x) {
+    $.ajax({
+        url: "http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=" + x,
+        dataType: "jsonp",
+        jsonpCallback: "jsonCallback"
+    });
+};
 
 function remove(item)
 {
