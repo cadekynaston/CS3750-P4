f189f6ef53aea8d2b0b5b0a4fea19d887bff2908
tim <Tim3Ds@gmail.com>
2017-04-24 17:41:53 -0600

    spacing

--- controllers/stock.js
+++ controllers/stock.js
@@ -25,7 +25,6 @@ router.get('/graphInfo', function(req, res, next){
   fetch('http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters=%7B%22Normalized%22%3Afalse%2C%22NumberOfDays%22%3A365%2C%22DataPeriod%22%3A%22Day%22%2C%22Elements%22%3A%5B%7B%22Symbol%22%3A%22AAPL%22%2C%22Type%22%3A%22price%22%2C%22Params%22%3A%5B%22c%22%5D%7D%5D%7D')
     .then(data => data.ok ? data.json() : Promise.reject())
     .then(data => res.send(data));
-  
 });
 router.get('/list', utils.requireLogin, function(req, res, next) {
 
