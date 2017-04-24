49bb9cf6f97ba031fd79d842761f8117bf5450dd
Alex <mustang.4222@yahoo.com>
2017-04-24 12:41:07 -0600

    styling

--- public/scripts/_list.js
+++ public/scripts/_list.js
@@ -3,40 +3,25 @@ window.onload = ()=> {
      var portfolio = JSON.parse($('#portfolio').val());
      portfolio.forEach(function(element) {
         console.log(element.stockCode);
-
-
-
+        
         $.ajax({
             url: "http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol="+ element.stockCode,
             dataType: "jsonp",
             success: function(data){
               let change = '';
                if (data.Change > 0) {
-                 change = 'up';
+                 change = 'Up';
                } else {
-                 change = 'down';
+                 change = 'Down';
                }
 
-              $('tbody').append('<tr><th>' + element.stockTitle + '</th><td>' + data.LastPrice + '</td>'
+              $('tbody').append('<tr><th>' + element.stockTitle + '</th><td>$' + data.LastPrice + '</td>'
               + '<td>'+change+'</td><td id="stockCode">' + element.stockCode + '</td><td><input type="button" id="removebtn" class="btn btn-danger" value="Remove" onclick="remove(this)"/></td>><hr/>');
             }
         });
     })
 }
 
-// function jsonCallback(data){
-//     console.log(data);
-//
-//     // add stockName to portfolio for easy access, maybe add current price too
-//     // they arent in the portfolio that's why they are undefined on the screen
-//     // $('tbody').append('<tr><th>' + element.stockTitle + '</th><td>' + element.price + '</td>'
-//     // + '<td>Up</td><td id="stockCode">' + element.stockCode + '</td><td><input type="button" id="removebtn" class="btn btn-danger" value="Remove" onclick="remove(this)"/></td>><hr/>');
-//
-// };
-
-function callAPI(x) {
-
-};
 
 function remove(item)
 {
