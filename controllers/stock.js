dd11e4f91fc77aab4684e032cb2c1a7e5eaa34a1
JedKrause <jedkrause@mail.weber.edu>
2017-04-24 19:54:31 -0600

    Added different API prototype to test
    look in stock.js to find the prototype to play with

--- controllers/stock.js
+++ controllers/stock.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var fetch = require('node-fetch');
 var router = express.Router();
+var yahooFinance = require('yahoo-finance');
+
 // has function to make sure you can only get to page if logged in
 
 var utils = require('./utils');
@@ -21,11 +23,16 @@ router.get('/view', utils.requireLogin, function(req, res, next) {
     csrfToken: req.csrfToken()
   });
 });
+
+
+
+
 router.get('/graphInfo/:symbol', function(req, res, next){
   console.log(req.params.symbol)
-  fetch('http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters=%7B%22Normalized%22%3Afalse%2C%22NumberOfDays%22%3A365%2C%22DataPeriod%22%3A%22Day%22%2C%22Elements%22%3A%5B%7B%22Symbol%22%3A%22'+req.params.symbol+'%22%2C%22Type%22%3A%22price%22%2C%22Params%22%3A%5B%22c%22%5D%7D%5D%7D')
-    .then(data => data.ok ? data.json() : Promise.reject())
-    .then(data => res.send(data));
+   console.log(yahooFinance.snapshot({
+     symbol: req.params.Symbol,
+     fields: ['m']
+   }));
 });
 router.get('/list', utils.requireLogin, function(req, res, next) {
 
