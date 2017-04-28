d1791def4f7ea9e2866f223cee5206176833cf4f
tim <Tim3Ds@gmail.com>
2017-04-27 20:59:21 -0600

    progress

--- public/scripts/_stock.js
+++ public/scripts/_stock.js
@@ -1,88 +1,94 @@
 window.onload = ()=>{
-    var seriesOptions = [],
-        seriesCounter = 0,
-        names = [];
+    
+        
     var portfolio = JSON.parse($('#portfolio').val());
+    
     portfolio.forEach(function(element) {
         console.log(element.stockCode);
-        names.push(element.stockCode);
-    });
-
-    /**
-     * Create the chart when all data is loaded
-     * @returns {undefined}
-     */
-
-    $.each(names, function (i, name) {
-
+        let name = element.stockCode;
+        let data = [];
         $.get('/stock/graphInfo/'+name, function (text) {
             console.log(text.substring(30,text.length-2));
             let json = JSON.parse(text.substring(30,text.length-2));
-            let data = [];
             $.each(json.series,function(i, series){
                 let time = series.Timestamp/.001
-                //time = time.toLocaleDateString() + ' ' + time.toLocaleTimeString();
+                // 1493348187939 your time
+                // 1491573298000 my time
                 data.push({
                     x:time,
                     y:series.high
                 });
             })
-            console.log('index', i , 'data', data);
-            var iChart = "c";
-            iChart += name.toString();
-            $('#container').append("<div id=" + iChart + "></div>");
-            $('#' + iChart).append("<p>test: " + name + "</p>");
+            console.log(data)
             
-            function doIt(div){
-                $('#' + div).highcharts({
-                    chart: {
-                        type: 'spline',
-                        animation: Highcharts.svg,
-                        marginRight: 
-                        10,
-                    },
-                    title: {
-                        text: name
-                    },
-                    xAxis: {
-                        type: 'datetime',
-                        tickPixelInterval: 150
-                    },
-                    yAxis: {
-                        title: {
-                            text: 'Value'
-                        },
-                        plotLines: [{
-                            value: 0,
-                            width: 1,
-                            color: '#808080'
-                        }]
-                    },
-                    tooltip: {
-                        formatter: function () {
-                            return '<b>' + this.series.name + '</b><br/>' +
-                                Highcharts.dateFormat('%b %d %Y %H:%M:%S', this.x) + '<br/>' +
-                                Highcharts.numberFormat(this.y, 2);
-                        }
-                    },
-                    legend: {
-                        enabled: false
-                    },
-                    exporting: {
-                        enabled: false
-                    },
-                    series:{
-                        name: name,
-                        data: data
-                    }
-                });
-
-            }
+        })
         
-            doIt(iChart);
+        var iChart = "c";
+        iChart += name.toString();
+        $('#container').append("<div id=" + iChart + "></div>");
+        $('#' + iChart).append("<p>test: " + name + "</p>");
+        
+        function doIt(div){
+            $('#' + div).highcharts({
+            chart: {
+                type: 'spline',
+                animation: Highcharts.svg,
+                marginRight: 
+                10,
+            },
+            title: {
+                text: name
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 150
+            },
+            yAxis: {
+                title: {
+                    text: 'Value'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + '</b><br/>' +
+                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                        Highcharts.numberFormat(this.y, 2);
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            exporting: {
+                enabled: false
+            },
+            series: [{
+                name: 'Random data',
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
 
-            
+                    for (i = -19; i <= 0; i += 1) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: Math.random()
+                        });
+                    }
+                    console.log(data);
+                    return data;
+                }())
+            }]
         });
+
+        }
+        doIt(iChart);
     });
+    
 }
     
