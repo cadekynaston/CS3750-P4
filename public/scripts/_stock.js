3953180a918362c8aa255a7c76a5bab021dbbe34
tim <Tim3Ds@gmail.com>
2017-04-27 21:33:51 -0600

    sorta working

--- public/scripts/_stock.js
+++ public/scripts/_stock.js
@@ -14,10 +14,11 @@ window.onload = ()=>{
                 let time = series.Timestamp/.001
                 // 1493348187939 your time
                 // 1491573298000 my time
-                data.push({
-                    x:time,
-                    y:series.high
-                });
+                if(i%3==1)
+                    data.push({
+                        x:time,
+                        y:series.high
+                    });
             })
             console.log(data)
             var iChart = "c";
@@ -25,48 +26,31 @@ window.onload = ()=>{
             $('#container').append("<div id=" + iChart + "></div>");
             $('#' + iChart).append("<p>test: " + name + "</p>");
             function doIt(div){
-                $('#' + div).highcharts({
-                    chart: {
-                        type: 'spline',
-                        animation: Highcharts.svg,
-                        marginRight: 
-                        10,
+                Highcharts.stockChart(div, {
+
+
+                    rangeSelector: {
+                        selected: 1
                     },
+
                     title: {
                         text: name
                     },
-                    xAxis: {
-                        type: 'datetime',
-                        tickPixelInterval: 150
-                    },
-                    yAxis: {
-                        title: {
-                            text: 'Value'
-                        },
-                        plotLines: [{
-                            value: 0,
-                            width: 1,
-                            color: '#808080'
-                        }]
-                    },
-                    tooltip: {
-                        formatter: function () {
-                            return '<b>' + this.series.name + '</b><br/>' +
-                                Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
-                                Highcharts.numberFormat(this.y, 2);
-                        }
-                    },
-                    legend: {
-                        enabled: false
-                    },
-                    exporting: {
-                        enabled: false
-                    },
+
                     series: [{
                         name: name,
-                        data: data
+                        data: data,
+                        tooltip: {
+                            valueDecimals: 2,
+                            formatter: function () {
+                                return '<b>' + this.series.name + '</b><br/>' +
+                                    Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                                    Highcharts.numberFormat(this.y, 2);
+                            }
+                        }
                     }]
                 });
+            
 
             }
             doIt(iChart)
