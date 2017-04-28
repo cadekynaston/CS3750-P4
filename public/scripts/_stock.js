a571acaa7e2e68f8e16ceb25886b744802552e8b
tim <Tim3Ds@gmail.com>
2017-04-27 19:16:04 -0600

    remove highstock

--- public/scripts/_stock.js
+++ public/scripts/_stock.js
@@ -13,44 +13,6 @@ window.onload = ()=>{
      * @returns {undefined}
      */
 
-    function createChart() {
-
-        Highcharts.stockChart('container', {
-
-            rangeSelector: {
-                selected: 4
-            },
-
-            yAxis: {
-                labels: {
-                    formatter: function () {
-                        return (this.value > 0 ? ' + ' : '') + this.value + '%';
-                    }
-                },
-                plotLines: [{
-                    value: 0,
-                    width: 2,
-                    color: 'silver'
-                }]
-            },
-
-            plotOptions: {
-                series: {
-                    compare: 'percent',
-                    showInNavigator: true
-                }
-            },
-
-            tooltip: {
-                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
-                valueDecimals: 2,
-                split: true
-            },
-
-            series: seriesOptions
-        });
-    }
-
     $.each(names, function (i, name) {
 
         $.get('/stock/graphInfo/'+name, function (text) {
@@ -70,9 +32,7 @@ window.onload = ()=>{
             // we keep a counter and create the chart when all the data is loaded.
             seriesCounter += 1;
 
-            if (seriesCounter === names.length) {
-                createChart();
-            }
+            
         });
     });
 }
